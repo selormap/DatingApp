f3a6c84ee56a12e0b48e1daa7e228e7f04e0a862
selorm apaloo <selormkwe@yahoo.com>
2019-02-14 16:16:17 +0000

    End of Section 12

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
@@ -7,6 +8,24 @@ namespace DatingApp.API.Dtos
         [Required]
         public string Username { get; set; }   
         [Required]
-        public string Password { get; set; }      
+        public string Password { get; set; }  
+         [Required]  
+        public string Gender { get; set; }  
+         [Required]
+        public string KnownAs { get; set; }
+         [Required]
+        public DateTime DateOfBirth { get; set; }
+         [Required]
+        public string City { get; set; }
+         [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
